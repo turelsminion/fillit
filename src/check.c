f8fa1c58de16e401ed3d97cd18507b0ba6549928
turelsminion <turelsminion19@gmail.com>
2018-09-10 21:55:05 +0300

    fillit

--- src/check.c
+++ src/check.c
@@ -6,11 +6,12 @@
 /*   By: andmiron <andmiron@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/29 15:36:31 by andmiron          #+#    #+#             */
-/*   Updated: 2018/09/01 12:13:20 by andmiron         ###   ########.fr       */
+/*   Updated: 2018/09/10 21:54:47 by andmiron         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fillit.h"
+#define IT(aux, i, fd, str) ((aux = 0) && (i = 0) && (fd = open(str, O_RDONLY)))
 
 static int	check_sym(char *str)
 {
@@ -28,9 +29,7 @@ static int	check_col(char *str)
 	int i;
 
 	i = 4;
-	if (str[20] != '\n')
-		return (0);
-	while (i != 24)
+	while (i < 20)
 	{
 		if (str[i] != '\n')
 			return (0);
@@ -41,26 +40,34 @@ static int	check_col(char *str)
 
 int			check(char *str)
 {
+	char	*s;
+	int		i;
 	int		fd;
-	char	s[22];
+	char	c;
 	int		aux;
 
+	aux = 0;
+	i = 0;
+	s[20] = '\0';
 	fd = open(str, O_RDONLY);
 	if (fd < 2)
 		return (0);
-	aux = 0;
-	while (read(fd, s, 21))
+	while (read(fd, &c, 1))
 	{
-		ft_putstr(s);
-		aux++;
-		if (check_col(s) == 0 && aux == 27)
+		if (i == 20)
 		{
-			close(fd);
-			return (0);
+			ft_putstr(s);
+			if (check_col(s) == 0 || aux > 26)
+				return (0);
+			read(fd, &c, 1);
+			i = 0;
+			aux++;
+		}
+		else
+		{
+			s[i] = c;
+			i++;
 		}
 	}
-	if (aux == 0)
-		return (0);
-	close(fd);
-	return (1);
+	return (check_col(s));
 }
